ccc3c008c35f656b54e1c382664f9ea8ecf3d162
thanhhoax701 <tth1610.vl@gmail.com>
2022-02-12 16:44:13 +0700

    add file Learn Eri

--- LearnEri/ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ LearnEri/ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.listview;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -56,8 +58,10 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> listData;
     private ArrayAdapter<String> adapter;
     private Context context;
-    private Button button;
+    private Button button, btnCapNhat;
     private EditText editText;
+    // Do mảng bắt đầu từ 0 nên để mặc định là -1
+    private int vitri = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         // Ánh xạ (lấy ra) button và editText
         button = (Button) findViewById(R.id.btn);
         editText = (EditText) findViewById(R.id.edt);
+        btnCapNhat = (Button) findViewById(R.id.btnCapNhat);
 
         listData = new ArrayList<>();
         listData.add("Bài 1: Giới thiệu về Android");
@@ -99,7 +104,48 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(context, listData.get(i), Toast.LENGTH_SHORT).show();
+                // Hiển thị dạng Toast ra màn hình
+                // Toast.makeText(context, listData.get(i), Toast.LENGTH_SHORT).show();
+
+                // Hiển thị lên phần nhập text
+                editText.setText((listData.get(i)));
+                vitri = i;
+            }
+        });
+
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listData.set(vitri, editText.getText().toString());
+                // Cập nhật lại dữ liệu
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        // Nhấn giữ vào item để xóa
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                dialog.setTitle("Xác nhận");
+                dialog.setMessage("Bạn có đồng ý xóa không?");
+                dialog.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        listData.remove(position);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                // Nếu không đồng ý xóa thì đóng dialog lại
+                dialog.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialog.setCancelable(true);
+                    }
+                });
+                AlertDialog alertDialog = dialog.create();
+                alertDialog.show();
+                return false;
             }
         });
     }
