abf3eeed83c351d4ca2d9bd102a13d25839d95c1
thanhhoax701 <tth1610.vl@gmail.com>
2022-02-28 13:21:09 +0700

    sửa lặt vặt

--- CT274/BTTH/buoi2/B2_B3_TranThanhHoa_B1908387/app/src/main/java/com/example/b2_b3_tranthanhhoa_b1908387/ThreadView.java
+++ CT274/BTTH/buoi2/B2_B3_TranThanhHoa_B1908387/app/src/main/java/com/example/b2_b3_tranthanhhoa_b1908387/ThreadView.java
@@ -4,9 +4,9 @@ import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 
 public class ThreadView extends Thread{
-    private GamePanel mPanel; // Khai báo 1 thể hiện của lớp Gamepanel
-    private SurfaceHolder mHolder; //Khai báo 1 đối tượng quản lý giao diện mức thấp
-    private boolean mRun = false; // Khai báo biền điều kiện cho vòng lặp while
+    private GamePanel mPanel;
+    private SurfaceHolder mHolder;
+    private boolean mRun = false;
 
     public ThreadView(GamePanel mPanel) {
         this.mPanel = mPanel;
@@ -21,7 +21,8 @@ public class ThreadView extends Thread{
     public void run() {
         Canvas canvas = null;
         while (mRun){
-            canvas = mHolder.lockCanvas(); // Hàm lockCanvas () tạo một canvas để vẽ
+            // Hàm lockCanvas () tạo một canvas để vẽ
+            canvas = mHolder.lockCanvas();
             if(canvas != null) {
                 mPanel.doDraw(canvas); //Thực hiện hành động trên canvas (hiển thị)
                 mHolder.unlockCanvasAndPost(canvas); //Hoàn thành việc vẽ trên canvas
