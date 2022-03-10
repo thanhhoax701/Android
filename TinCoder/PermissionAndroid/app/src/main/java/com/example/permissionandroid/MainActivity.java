e7c1cfdf7a0e29be2082815ff21ffd6b3058c788
thanhhoax701 <tth1610.vl@gmail.com>
2022-03-10 15:49:49 +0700

    Request Permission

---
+++ TinCoder/PermissionAndroid/app/src/main/java/com/example/permissionandroid/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.permissionandroid;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_CODE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRequestPermission = (Button) findViewById(R.id.btnRequestPermission);
+        Button btnOpenSettingPermission = (Button) findViewById(R.id.btnOpenSettingPermission);
+
+        btnRequestPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickRequestPermission();
+            }
+        });
+
+        btnOpenSettingPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void clickRequestPermission() {
+        // M là API 23, là từ Android 6
+        // Nếu Android nhỏ hơn thì không cần check như vầy
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return;
+        }
+        // Tiếp theo nếu từ Android 6 trở lên thì sẽ làm như sau
+        // Nếu 2 thằng đó bằng thì quyền đã được cấp phép rồi
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permission, REQUEST_PERMISSION_CODE);
+        }
+    }
+
+    // Khi người dùng cho phép hoặc từ chối
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+//    private void openSettingPermission() {
+//        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+//        Uri uri = Uri.fromParts("package", getPackageName(), null);
+//        intent.setData(uri);
+//        startActivity(intent);
+//    }
+
+}
