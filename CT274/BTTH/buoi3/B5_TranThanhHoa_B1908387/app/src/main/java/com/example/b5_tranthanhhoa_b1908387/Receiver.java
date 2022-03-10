cdbcce3d6052292a1a8db88bc46cef1b57919777
thanhhoax701 <tth1610.vl@gmail.com>
2022-03-10 15:50:40 +0700

    xong buổi 3

--- CT274/BTTH/buoi3/B5_TranThanhHoa_B1908387/app/src/main/java/com/example/b5_tranthanhhoa_b1908387/Receiver.java
+++ CT274/BTTH/buoi3/B5_TranThanhHoa_B1908387/app/src/main/java/com/example/b5_tranthanhhoa_b1908387/Receiver.java
@@ -9,10 +9,7 @@ public class Receiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         long value = intent.getLongExtra("new value", -10) + 10;
-        Toast toast = Toast.makeText(context, "Broadcast Receiver catch an Intent"
-                + " \n" + "The value is stored in the Intent is "
-                + String.valueOf(value), Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(context, "Broadcast Receiver catch an Intent" + " \n" + "The value is stored in the Intent is " + String.valueOf(value), Toast.LENGTH_LONG);
         toast.show();
     }
-
 }
